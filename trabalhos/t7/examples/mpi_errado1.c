6760ca22a061c35a6e9bb5ef371de93455aa4080
rwfazul <rwfazul@inf.ufsm.br>
2018-05-26 16:14:09 -0300

    mv to examples folder

---
+++ trabalhos/t7/examples/mpi_errado1.c
@@ -0,0 +1,44 @@
+//
+//  Este programa MPI tem um erro!
+//  Identifique este erro e corrija-o para que 2 processos
+//  possam trocar mensagens.
+//  
+//  Uso: mpirun -np 2 <prog>
+//  
+#include "mpi.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc,char *argv[])
+{
+   int numtasks, rank, dest, tag, source, rc;
+   int inmsg, outmsg = 100;
+   MPI_Status stat;
+
+   MPI_Init(&argc,&argv);
+   MPI_Comm_size(MPI_COMM_WORLD, &numtasks);
+   MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+
+   printf("Processo %d iniciando...\n",rank);
+
+   if (rank == 0) {
+      dest = 1;
+      source = dest;
+      tag = rank;
+      rc = MPI_Send(&outmsg, 1, MPI_INT, dest, tag, MPI_COMM_WORLD);
+      printf("Enviei mensagem para processo %d...\n", dest);
+      rc = MPI_Recv(&inmsg, 1, MPI_INT, source, tag, MPI_COMM_WORLD, &stat);
+      printf("Recebi mensagem do processo %d...\n", source);
+   }
+   else if (rank == 1) {
+      dest = 0;
+      source = dest;
+      tag = rank;
+      rc = MPI_Recv(&inmsg, 1, MPI_INT, source, tag, MPI_COMM_WORLD, &stat);
+      printf("Recebi mensagem do processo %d...\n", source);
+      rc = MPI_Send(&outmsg, 1, MPI_INT, dest, tag, MPI_COMM_WORLD);
+      printf("Enviei mensagem para processo %d...\n", dest);
+   }
+
+   MPI_Finalize();
+}
